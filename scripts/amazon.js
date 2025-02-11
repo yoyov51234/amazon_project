2a7d3e56d252df6304743fbf243240fddb60f1ff
Major <yoyov5123@163.com>
2025-02-11 15:43:20 +0800

    use modules and import cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import { cart } from "../data/cart.js";
+//here  we can use alias to name cart to avoid naming conflicts import { cart as myCart} from "../data/cart.js";
+
 let productsHTML = "";
 
 products.forEach((product) => {
