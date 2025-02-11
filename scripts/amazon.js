b1afd7f744b937b8e73a55beee7fda5210ee67ee
Major <yoyov5123@163.com>
2025-02-11 17:00:03 +0800

    make a new module and update the options in checkout html

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
 import { cart, addtoCart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./untils/money.js";
 //here  we can use alias to name cart to avoid naming conflicts import { cart as myCart} from "../data/cart.js";
 //import的另一种语法  import * as cartModule from "../data/cart.js";  这样会把所有在cart.js里面注明export的都import 进来
 
@@ -28,9 +29,7 @@ products.forEach((product) => {
             }</div>
           </div>
 
-          <div class="product-price">${(product.priceCents / 100).toFixed(
-            2
-          )}</div>
+          <div class="product-price">${formatCurrency(product.priceCents)}</div>
 
           <div class="product-quantity-container">
             <select class="js-quantity-selector-${
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from "../data/cart.js";
 import { products } from "../data/products.js";
+import { formatCurrency } from "./untils/money.js";
 
 let cartSummaryHtml = "";
 
@@ -29,9 +30,9 @@ cart.forEach((cartItem) => {
 
               <div class="cart-item-details">
                 <div class="product-name">${matchingProduct.name}</div>
-                <div class="product-price">$${(
-                  Number(matchingProduct.priceCents) / 100
-                ).toFixed(2)}</div>
+                <div class="product-price">$${formatCurrency(
+                  matchingProduct.priceCents
+                )}</div>
                 <div class="product-quantity">
                   <span> Quantity: <span class="quantity-label">${
                     cartItem.quantity
@@ -51,21 +52,27 @@ cart.forEach((cartItem) => {
                 </div>
 
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-2">
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${
+                    matchingProduct.id
+                  }">
                   <div>
                     <div class="delivery-option-date">Tuesday, June 21</div>
                     <div class="delivery-option-price">FREE Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" checked="" class="delivery-option-input" name="delivery-option-2">
+                  <input type="radio" checked="" class="delivery-option-input" name="delivery-option-${
+                    matchingProduct.id
+                  }">
                   <div>
                     <div class="delivery-option-date">Wednesday, June 15</div>
                     <div class="delivery-option-price">$4.99 - Shipping</div>
                   </div>
                 </div>
                 <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-2">
+                  <input type="radio" class="delivery-option-input" name="delivery-option-${
+                    matchingProduct.id
+                  }">
                   <div>
                     <div class="delivery-option-date">Monday, June 13</div>
                     <div class="delivery-option-price">$9.99 - Shipping</div>
